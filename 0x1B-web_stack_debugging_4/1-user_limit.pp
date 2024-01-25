b6cc1b33fc7a1b622556fb01b59f4fd87b8ee7b6
ZAk-27 <touhadi.zakaria@gmail.com>
2024-01-25 13:10:27 -0800

    updte

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
 # Fix problem of high amount files opened
 exec { 'change_config':
-  command => "sudo /bin/sed -i 's/nofile 4/nofile 5000/g; s/nofile 5/nofile 6000/g' /etc/security/limits.conf",
+  command => "sudo /bin/sed -i 's/nofile 4/nofile 4000/g; s/nofile 5/nofile 6000/g' /etc/security/limits.conf",
 }
