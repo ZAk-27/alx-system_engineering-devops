d33f48230f8aadc56c9e5a7ddac3cee698d7f70f
ZAk-27 <touhadi.zakaria@gmail.com>
2024-02-28 16:00:51 -0800

    up

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,12 @@
-# Fix problem of high amount files opened
-exec { 'change_config':
-  command => "sudo /bin/sed -i 's/nofile 4/nofile 4000/g; s/nofile 5/nofile 6000/g' /etc/security/limits.conf",
+# Update limits
+
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
